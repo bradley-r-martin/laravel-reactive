b19807199c10648a37e1d3b8743560eb23f177c8
Bradley Martin <bradley.r.martin@me.com>
2022-03-16 19:48:21 +1100

    update

--- src/Traits/Authenticated.php
+++ src/Traits/Authenticated.php
@@ -1,12 +1,13 @@
 <?php
 
 namespace Sihq\Traits;
+use Sihq\Facades\Payload;
 
 trait Authenticated
 {
 
-    public function __construct(){
-        parent::__construct();
+    public function __construct(Payload $payload){
+        parent::__construct($payload);
         if(!auth()->user()){
             $this->redirect('/');
         }
